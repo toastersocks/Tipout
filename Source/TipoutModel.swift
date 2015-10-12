d0e11c0711a7189b7c276e23da12a48b158c1c27
James Pamplona <pamplonajames@gmail.com>
2015-10-12 16:30:00 -0700

    Moved TipoutMethod into its own file

--- Source/TipoutModel.swift
+++ Source/TipoutModel.swift
@@ -6,68 +6,6 @@
 //  Copyright (c) 2015 James Pamplona. All rights reserved.
 //
 
-import UIKit
-
-
-public enum TipoutMethod {
-    case Percentage(Double)
-    case Amount(Double)
-    case Hourly(Double)
-    case Function(() -> Double)
-}
-
-extension TipoutMethod: Equatable {}
-
-public func ==(lhs: TipoutMethod, rhs: TipoutMethod) -> Bool {
-    switch (lhs, rhs) {
-    case (.Percentage(let leftValue), .Percentage(let rightValue)) where leftValue == rightValue:
-        return true
-    case (.Amount(let leftValue), .Amount(let rightValue)) where leftValue == rightValue:
-        return true
-    case (.Hourly(let leftValue), .Hourly(let rightValue)) where leftValue == rightValue:
-        return true
-    case (.Function(let leftValue), .Function(let rightValue)) where leftValue() == rightValue():
-        return true
-    default:
-        return false
-    }
-}
-
-extension TipoutMethod: Hashable {
-    public var hashValue: Int {
-        switch self {
-        case .Percentage(let value):
-            return 0.hashValue ^ value.hashValue
-        case .Amount(let value):
-            return 1.hashValue ^ value.hashValue
-        case .Hourly(let value):
-            return 2.hashValue ^ value.hashValue
-        case .Function(let value):
-            return 3.hashValue ^ value().hashValue
-        }
-    }
-}
-
-extension TipoutMethod: CustomDebugStringConvertible {
-    public var debugDescription: String {
-        var descString = ""
-//        print("TipoutMethod.", terminator: "", toStream: &descString)
-        print(".", terminator: "", toStream: &descString)
-
-        switch self {
-        case Percentage(let percent):
-            print("Percentage(\(percent))", toStream: &descString)
-        case .Amount(let amount):
-            print("Amount(\(amount))", toStream: &descString)
-        case .Hourly(let hours):
-            print("Hourly(\(hours))", toStream: &descString)
-        case .Function(let f):
-            print("Function(\(f) (With a value of \(f()))", toStream: &descString)
-        }
-        
-        return descString
-    }
-}
 
 public func +(lhs: TipoutModel, rhs: TipoutModel) -> TipoutModel {
     return lhs.combineWith(rhs)
